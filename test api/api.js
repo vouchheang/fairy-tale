bc28bc003f33277352ad920ab286f123adf0ed7c
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-03-27 14:23:41 +0700

    done component

---
+++ test api/api.js
@@ -0,0 +1,6 @@
+fetch('https://fairy-tale-api-inky.vercel.app/api/fairytales')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+
+f
